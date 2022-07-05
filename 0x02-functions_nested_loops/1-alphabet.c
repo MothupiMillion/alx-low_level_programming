994e11f69a3d18fdd14083bbcfcfa7cac5f690e5
MothupiMillion <mothupimillion@outlook.com>
2022-07-05 10:05:22 -0700

    Commit A

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
+* print_alphabet - prints the alphabet, in lowercase followed by a new line
 *
-*
+* Return: no return value
 */
 void print_alphabet(void)
 {
-int j;
-
-for (j= 'a' ; j <= 'z' ; j++)
-    _putchar(j);
-}
+char c = 'a';
+while (c <= 'z')
+_putchar(c++);
 _putchar('\n');
+}
