3865225e76824d0bd639a2eb6c98315a78174f01
MothupiMillion <mothupimillion@outlook.com>
2022-07-07 03:30:20 -0700

    Changed

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,13 +4,12 @@
 * print_numbers - function that prints the numbers, from 0 to 9.
 * @void: no return
 */
-
 void print_numbers(void)
 {
-char i;
-for (i = '0'; i <= '9'; i++)
+int ch;
+for (ch = 48; ch < 58; ch++)
 {
-_putchar(i);
+_putchar(ch);
 }
 _putchar('\n');
 }
