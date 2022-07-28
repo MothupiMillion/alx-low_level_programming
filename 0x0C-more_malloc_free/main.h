d99e700787ba6605d5bbdd824e65e03d6037ae17
MothupiMillion <mothupimillion@outlook.com>
2022-07-28 09:44:09 -0700

    bababababa

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,6 +12,6 @@ int *array_range(int min, int max);
 void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 #endif
