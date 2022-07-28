2b811817e06a1bfe56b9b9da99737485272e0e0c
MothupiMillion <mothupimillion@outlook.com>
2022-07-28 04:00:54 -0700

    lkjhgfdsa

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,5 @@
-
 #include "main.h"
+
 /**
 * main - multiplies two positive numbers
 * @argc: n arguments
@@ -21,6 +21,7 @@ if (argv[i][j] > 57 || argv[i][j] < 48)
 {  printf("Error\n");
 exit(98); }
 }
+
 }
 mul = atol(argv[1]) *  atol(argv[2]);
 printf("%lu\n", mul);
