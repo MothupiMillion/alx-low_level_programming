0d40e82ccf91f77328ad7aa4c487f9fcae6064b5
Brandon Mothupi <107117491+MothupiMillion@users.noreply.github.com>
2022-08-22 17:52:33 +0200

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,12 +17,13 @@ fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
 if (text_content)
-{      
-for (nletters = 0; text_content[nletters]; nletters++);
+{
+for (nletters = 0; text_content[nletters]; nletters++)
+;
 rwr = write(fd, text_content, nletters);
 if (rwr == -1)
 return (-1);
 }
 close(fd);
-return (1); 
+return (1);
 }
